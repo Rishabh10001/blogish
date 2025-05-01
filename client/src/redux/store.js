cfbf7aa74db4181adb00d01aa6ab54be40abe28d
Rishabh Jain <rishabhjain@Rishabhs-MacBook-Air.local>
2025-05-01 23:15:33 +0530

    added dark mode function

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,9 +3,11 @@ import userReducer from './user/userSlice';
 import { persistStore, persistReducer } from 'redux-persist';
 import { version } from 'react';
 import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import themeReducer from './theme/themeSlice';
 
 const rootReducer = combineReducers({
     user: userReducer,
+    theme: themeReducer
 });
 
 const persistConfig = {
