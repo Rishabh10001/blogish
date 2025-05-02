0ff3ebf90d4ac10ba0061c50a5e0feb1da588a90
Rishabh Jain <rishabhjain@Rishabhs-MacBook-Air.local>
2025-05-03 00:29:35 +0530

    complete update user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -30,6 +30,7 @@ export const updateUser = async (req, res, next) => {
         if(!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
             return next(errorHandler(400, 'Username must only contain letters and numbers'))
         }
+    }
         try {
             const updatedUser = await User.findByIdAndUpdate(req.params.userId, {
                 $set: {
@@ -44,5 +45,5 @@ export const updateUser = async (req, res, next) => {
         } catch (error) {
             next(error)
         }
-    }
+    
 }
