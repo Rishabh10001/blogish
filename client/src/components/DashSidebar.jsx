2682ba4c3fcc4469b63593f5d17fddb50cb9745d
Rishabh Jain <rishabhjain@Rishabhs-MacBook-Air.local>
2025-05-03 07:12:38 +0530

    show users to admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,7 +1,7 @@
 import { Sidebar } from "flowbite-react";
 import React from "react";
 import { useState, useEffect } from "react";
-import { HiArrowSmRight, HiDocumentText, HiUser } from 'react-icons/hi'
+import { HiAnnotation, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser } from 'react-icons/hi'
 import { Link, useLocation } from "react-router-dom";
 import { signoutSuccess } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
@@ -55,6 +55,28 @@ export default function DashSidebar() {
                         Posts
                     </Sidebar.Item>
                     </Link> )}
+                    {currentUser.isAdmin && (
+                            <>
+                            <Link to='/dashboard?tab=users'>
+                                <Sidebar.Item
+                                active={tab === 'users'}
+                                icon={HiOutlineUserGroup}
+                                as='div'
+                                >
+                                Users
+                                </Sidebar.Item>
+                            </Link>
+                            <Link to='/dashboard?tab=comments'>
+                                <Sidebar.Item
+                                active={tab === 'comments'}
+                                icon={HiAnnotation}
+                                as='div'
+                                >
+                                Comments
+                                </Sidebar.Item>
+                            </Link>
+                            </>
+                        )}
                     <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer' onClick={handleSignout}>
                         Sign Out
                     </Sidebar.Item>
