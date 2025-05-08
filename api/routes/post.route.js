41db4da55a71b1518490ad0ec576063e35acc1f4
Rishabh Jain <rishabhjain@Rishabhs-MacBook-Air.local>
2025-05-08 21:17:35 +0530

    edit post accessible

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -9,7 +9,7 @@ const router = express.Router();
 router.post("/create", create)
 router.get('/getposts', getposts)
 router.delete('/deletepost/:postId/:userId', verifyToken, deletepost)
-// router.put('/updatepost/:postId/:userId', verifyToken, updatepost)
-router.put('/updatepost/:postId/:userId', updatepost)
+router.put('/updatepost/:postId/:userId', verifyToken, updatepost)
+// router.put('/updatepost/:postId/:userId', updatepost)
 
 export default router;
