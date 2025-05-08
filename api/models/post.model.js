9539db551b92f2018e9f12a38142e5a871244f2b
Rishabh Jain <rishabhjain@Rishabhs-MacBook-Air.local>
2025-05-08 21:30:32 +0530

    fixed the bug

--- api/models/post.model.js
+++ api/models/post.model.js
@@ -2,10 +2,10 @@ import mongoose from 'mongoose';
 
 const postSchema = new mongoose.Schema(
   {
-    userId: {
-      type: String,
-      required: true,
-    },
+    // userId: {
+    //   type: String,
+    //   required: true,
+    // },
     content: {
       type: String,
       required: true,
